c473c6564cc5797911e4e0a3f4dc525f0a96268d
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2023-08-08 16:17:29 +0900

    Feat split directory

---
+++ src/components/components.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type ComponentsProps = {};
+
+const Components: React.FC<ComponentsProps> = ({}) => {
+  return <></>;
+};
+
+export default Components;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Components from "@/components/components";
+
+const Home: React.FC = () => {
+  return <Components />;
+};
+
+export default Home;
